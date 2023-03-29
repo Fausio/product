8e97122a60e4a73513eeb47ea7a77c2b87e01898
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-03-29 15:58:04 +0200

    ProductController works ✅

--- src/main/java/mz/com/api/product/controller/ProductController.java
+++ src/main/java/mz/com/api/product/controller/ProductController.java
@@ -1,5 +1,17 @@
 package mz.com.api.product.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class ProductController {
+
     
+    @GetMapping("/")
+    public String route(){
+
+        return "ProductController works ✅";
+    }
+
 }
