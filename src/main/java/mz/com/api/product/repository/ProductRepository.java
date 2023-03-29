101a2e6cf5de963554c84058e46823fa702530b0
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-03-29 15:55:43 +0200

    ProductRepository

--- src/main/java/mz/com/api/product/repository/ProductRepository.java
+++ src/main/java/mz/com/api/product/repository/ProductRepository.java
@@ -1,5 +1,12 @@
 package mz.com.api.product.repository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public class ProductRepository {
+import  mz.com.api.product.model.Product;
+ 
+ 
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product,Long>{
     
 }
