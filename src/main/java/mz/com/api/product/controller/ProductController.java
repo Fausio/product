b4974c5819193f4ed02c46e7c00cab0e1933d3bb
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-03-29 16:18:48 +0200

    ProductController

--- src/main/java/mz/com/api/product/controller/ProductController.java
+++ src/main/java/mz/com/api/product/controller/ProductController.java
@@ -1,15 +1,28 @@
 package mz.com.api.product.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import mz.com.api.product.model.Product;
+import mz.com.api.product.repository.ProductRepository;
+import mz.com.api.product.service.ProductService;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/read")
+    public Iterable<Product> read() {
+
+        return productService.read();
+    }
     
-    @GetMapping()
+    @GetMapping("/")
     public String route(){
 
         return "ProductController works ✅";
