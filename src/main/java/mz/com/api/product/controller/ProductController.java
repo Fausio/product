1ecdf5c2af78b73a28c0e5740bdfe353bb226108
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-03-29 16:34:37 +0200

    @PostMapping("/create")

--- src/main/java/mz/com/api/product/controller/ProductController.java
+++ src/main/java/mz/com/api/product/controller/ProductController.java
@@ -1,7 +1,10 @@
 package mz.com.api.product.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +24,15 @@ public class ProductController {
 
         return productService.read();
     }
-    
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody Product model) {
+
+        return productService.create(model);
+    }
+
     @GetMapping("/")
-    public String route(){
+    public String route() {
 
         return "ProductController works ✅";
     }
