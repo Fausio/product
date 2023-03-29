bbccc75ed9885b19ea77006dfa48b111c2000124
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-03-29 16:09:03 +0200

    productRepository

--- src/main/java/mz/com/api/product/service/ProductService.java
+++ src/main/java/mz/com/api/product/service/ProductService.java
@@ -1,5 +1,18 @@
 package mz.com.api.product.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import mz.com.api.product.model.Product;
+import mz.com.api.product.repository.ProductRepository;
+
+@Service
 public class ProductService {
-    
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Iterable<Product> read() {
+        return productRepository.findAll();
+    }
 }
